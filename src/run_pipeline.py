d1f62018f5f9c17aa3b6c376cf5e7e6984ab5c9a
maxxxmo <149115502+maxxxmo@users.noreply.github.com>
2026-01-18 15:50:45 +0100

    run_pipeline refactoring

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -3,44 +3,55 @@ from src.ingestion.load_data import load_data
 from src.preprocessing.preprocess import preprocess_data
 from src.training.training import train_model
 from src.utils.mlflow_utils import start_mlflow_experiment, log_model_run
-from sklearn.linear_model import LogisticRegression  # modèle spécifique
-# Before the add of mlflow to test pipeline core
 from src.evaluation.metrics_registry import METRICS_REGISTRY
-# from src.training.model_registry import MODEL_REGISTRY
+from sklearn.linear_model import LogisticRegression
 
-def main():
-    # data ingestion
 
-    print("Loading data...")
-    df = load_data("data/raw/titanic.csv")
+def run_pipeline(
+    data_path: str,
+    enable_mlflow: bool = True,
+):
+    # ingestion
+    df = load_data(data_path)
+
     # preprocessing
-    print("Preprocessing data...")
     X_train, X_test, y_train, y_test = preprocess_data(df)
 
-    # for training model
+    # training
     model_cls = LogisticRegression
     params = {"max_iter": 200}
-    
-    
-    print("Starting MLflow experiment...")
-    start_mlflow_experiment()
 
-    print("Training model...")
+    if enable_mlflow:
+        start_mlflow_experiment()
+
     model = train_model(model_cls, params, X_train, y_train)
 
-    print("Evaluating model...")
-    accuracy = evaluate_model(model, X_test, y_test, metrics_registry=METRICS_REGISTRY)
-    # metrics = {"accuracy": accuracy}
-    
-    
-    print(f"Model accuracy: {accuracy}")
+    # evaluation
+    metrics = evaluate_model(
+        model,
+        X_test,
+        y_test,
+        metrics_registry=METRICS_REGISTRY
+    )
 
-    print("Logging run to MLflow...")
-    metrics_float = {k: float(v) for k, v in accuracy.items()}
-    log_model_run(model, params=params, metrics=metrics_float, run_name="logreg_v1")
-    
-    
-    
-    
+    if enable_mlflow:
+        metrics_float = {k: float(v) for k, v in metrics.items()}
+        log_model_run(
+            model,
+            params=params,
+            metrics=metrics_float,
+            run_name="logreg_v1"
+        )
+
+    return metrics
+
+
+def main():
+    run_pipeline(
+        data_path="data/raw/titanic.csv",
+        enable_mlflow=True
+    )
+
+
 if __name__ == "__main__":
     main()
